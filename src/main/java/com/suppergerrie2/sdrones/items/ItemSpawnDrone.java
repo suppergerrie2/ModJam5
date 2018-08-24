54119668ba10a6e251d2a5fd67c99dd6c6e5274a
suppergerrie2 <gerjan2001@gmail.com>
2018-08-24 10:02:58 +0200

    Fixed drone not dropping spawn item if item was last of a stack

--- src/main/java/com/suppergerrie2/sdrones/items/ItemSpawnDrone.java
+++ src/main/java/com/suppergerrie2/sdrones/items/ItemSpawnDrone.java
@@ -18,44 +18,40 @@ import net.minecraft.world.World;
 public class ItemSpawnDrone<E extends EntityBasicDrone> extends ItemDrone {
 
 	Function<World, E> droneCreator;
-	
+
 	public ItemSpawnDrone(String name, ResourceLocation droneLocation, Function<World, E> droneCreator) {
 		super(name);
-		if(entityNameToItem.containsKey(droneLocation)) {
+		if (entityNameToItem.containsKey(droneLocation)) {
 			DroneMod.logger.warn("Already have spawn item for " + droneLocation.toString());
 		}
-		
+
 		entityNameToItem.put(droneLocation, this);
 		this.droneCreator = droneCreator;
 	}
-	
+
 	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand,
-			EnumFacing facing, float hitX, float hitY, float hitZ) {
-		
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+
 		ItemStack itemstack = player.getHeldItem(hand);
 
-		if (!player.capabilities.isCreativeMode)
-		{
-			itemstack.shrink(1);
-		}
-
-		if (!worldIn.isRemote)
-		{		
+		if (!worldIn.isRemote) {
 			ItemStack stack = itemstack.copy();
 			stack.setCount(1);
-			EntityBasicDrone entityDrone = droneCreator.apply(worldIn);
+			EntityBasicDrone entityDrone = this.droneCreator.apply(worldIn);
 			entityDrone.init(pos.getX() + hitX, pos.getY() + hitY, pos.getZ() + hitZ, stack, facing);
-			
+
 			DroneUpgrade.applyUpgradesFromStack(itemstack, entityDrone);
-			
-			if(this.hasFilter(stack)) {
+
+			if (this.hasFilter(stack)) {
 				entityDrone.setFilter(this.getFilter(itemstack));
 			}
-			
+
 			worldIn.spawnEntity(entityDrone);
 		}
 
+		if (!player.capabilities.isCreativeMode) {
+			itemstack.shrink(1);
+		}
 		return EnumActionResult.SUCCESS;
 	}
 
