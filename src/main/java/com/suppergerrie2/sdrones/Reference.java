ca488e41a85673c387501d6f9a473ed9a3fa857b
VirxEpicoder <ericmveilleux@gmail.com>
2019-03-02 13:58:53 -0500

    Update Reference.java
    Change this is have Forge know where to check for your update json and also change the accepted Minecraft versions so your mod runs in 1.12, 1.12.1, and 1.12.2.

--- src/main/java/com/suppergerrie2/sdrones/Reference.java
+++ src/main/java/com/suppergerrie2/sdrones/Reference.java
@@ -5,6 +5,7 @@ public class Reference {
 	public static final String MODID = "sdrones";
 	public static final String MODNAME = "Suppergerrie2's Drone Mod";
 	public static final String VERSION = "1.3.1";
-	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12][1.12.1][1.12.2]";
+	public static final String MOD_URL = "https://raw.githubusercontent.com/suppergerrie2/ModJam5/master/update.json";
 	public static final String UPGRADE_REGISTRY = "drone_upgrades";
 }
